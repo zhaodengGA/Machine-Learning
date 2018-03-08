8e7aa6d8cef30e75006b90e3e54c9bd9b95a67b2
Zhao Deng <zhaodeng@fusion.gat.com>
2018-03-08 09:59:38 +0800

    Update logistic.py

--- Logistics-Regression/logistic.py
+++ Logistics-Regression/logistic.py
@@ -10,15 +10,15 @@
 '''
         ** Logistic Regression **
 
-This is a logistic regression classifier example.
+* This is a logistic regression classifier which could treat the binary classification problem.
 
-Sigmoid function is applied as activation function. 
+* Sigmoid function is applied as activation function. 
 
-Cross entropy has been chosen as Cost Function. 
+* Cross entropy has been chosen as Cost Function. 
 
-The data are rescaled before training.
+* The data are rescaled before training so as to largely increase the converging efficiency.
 
-The gradient descent method is applied to train the weight theta.
+* The gradient descent method is applied to train the weight and bias.
 
 '''
 
